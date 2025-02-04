22ceb022c5d79a5e741c00f888ec94c6a7bc9bd2
yashrajmandloi <er.mandloiyashraj@gmail.com>
2025-02-04 13:16:38 -0500

    home page hero component complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import SearchForm from "@/components/SearchForm";
 
-const Home = () => {
-  return <div className="">Home</div>;
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) => {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      views: 100,
+      author: "John Doe",
+      _id: "1",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc nec ultricies ultricies, nunc nisl ultricies",
+      image:
+        "https://images.unsplash.com/photo-1630480003494-4b3b3b3b3b3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjIwNzN8MHwxfGFsbHwxf",
+      category: "Tech",
+      title: "Tech Fest",
+    },
+  ];
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup <br /> Connect With Community
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Pitch, Connect, and Grow—Turn Ideas Into Opportunities!
+        </p>
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
 };
 
 export default Home;
