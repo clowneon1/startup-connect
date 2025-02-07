da8d1518f086d778e36209d1bcde0a18bb701c1a
yashrajmandloi <er.mandloiyashraj@gmail.com>
2025-02-07 13:32:44 -0500

    add live count update feature on page visit

--- components/View.tsx
+++ components/View.tsx
@@ -3,13 +3,21 @@ import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { formatViews } from "@/lib/utils";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_BY_ID_QUERY, { id });
 
-  //TODO: Update the view count on page visit
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
