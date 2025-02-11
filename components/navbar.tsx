2dfdadd0a4d5da998fa94817f1cc17e00df20da8
yashrajmandloi <er.mandloiyashraj@gmail.com>
2025-02-11 13:56:45 -0500

    add responsiveness to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
 import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = async () => {
   const session = await auth();
@@ -22,7 +24,8 @@ const Navbar = async () => {
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden text-green-500" />
               </Link>
 
               <form
@@ -32,11 +35,18 @@ const Navbar = async () => {
                 }}
               >
                 <button type="submit">
-                  <span>Logout</span>
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
                 </button>
               </form>
               <Link href={`/user/${session?.user?.id}`}>
-                <span>{session?.user?.name}</span>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
